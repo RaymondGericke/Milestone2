d0975b19f1df45b5bd321c65e14baa9bb8c404b3
RaymondGericke <576839@student.belgiumcampus.ac.za>
2021-10-11 20:08:30 +0200

    Create qryMilestone2.sql
    Created SQLqry

---
+++ PRG282_Project2/Cassidy_Potgieter_Raymond_Gericke_Sebastian_Marnewick_PRG282_Project2/qryMilestone2.sql
@@ -0,0 +1,88 @@
+﻿CREATE DATABASE Milestone2;
+
+USE Milestone2
+Go
+
+CREATE Table Students
+(
+	StudentNumber INT NOT NULL,
+	sName VARCHAR(100),
+	Surname VARCHAR(100),
+	StudentIMG VARBINARY(MAX),
+	DOB DATE,
+	GENDER VARCHAR(20),
+	PhoneNo VARCHAR(11),
+	Address VARCHAR(100),
+	ModCodes VARCHAR(8)
+)
+
+GO
+CREATE PROCEDURE spGetStudents
+AS
+BEGIN
+	SELECT * FROM Students
+END
+
+GO
+EXECUTE spGetStudents
+
+GO
+CREATE PROCEDURE spAddStudent
+(
+	@StudentNumber INT,
+	@sName VARCHAR(100),
+	@Surname VARCHAR(100),
+	@StudentIMG VARBINARY(MAX),
+	@DOB DATE,
+	@GENDER VARCHAR(20),
+	@PhoneNo VARCHAR(11),
+	@Address VARCHAR(100),
+	@ModCodes VARCHAR(8)
+)
+AS
+BEGIN
+	INSERT INTO Students
+	VALUES (@StudentNumber,@sName, @Surname, @StudentIMG, @DOB, @GENDER, @PhoneNo, @Address, @ModCodes)
+END
+
+GO
+CREATE PROCEDURE spUpdateStudent
+(
+	@StudentNumber INT,
+	@sName VARCHAR(100),
+	@Surname VARCHAR(100),
+	@StudentIMG VARBINARY(MAX),
+	@DOB DATE,
+	@GENDER VARCHAR(20),
+	@PhoneNo VARCHAR(11),
+	@Address VARCHAR(100),
+	@ModCodes VARCHAR(8)
+)
+AS
+BEGIN
+	UPDATE Students
+	SET StudentNumber = @StudentNumber, sName = @sName, Surname = @Surname, StudentIMG = @StudentIMG, DOB = @DOB, GENDER = @GENDER, PhoneNo =  @PhoneNo, Address = @Address, ModCodes = @ModCodes
+	WHERE @StudentNumber = @StudentNumber
+END
+
+GO
+CREATE PROCEDURE spDeleteStudent
+(
+	@StudentNumber INT
+)
+AS
+BEGIN
+	DELETE FROM Students
+	WHERE StudentNumber = @StudentNumber
+END
+
+GO
+CREATE PROCEDURE spSearchStudent
+(
+	@StudentNumber INT
+)
+AS
+BEGIN
+	SELECT * FROM Students
+	WHERE StudentNumber = @StudentNumber 
+END
